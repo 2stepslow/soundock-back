3f39ff6847b884d32b98b295ccb6e98076c07e56
ningsoo <xkfkr13@gmail.com>
2026-01-07 16:24:42 +0900

    invite

---
+++ src/test/java/dopamine/soundock/SoundockApplicationTests.java
@@ -0,0 +1,13 @@
+package dopamine.soundock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
