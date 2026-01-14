6bbcb02ff41706ae68d238b9a88dda69b568e668
ning <xkfkr13@gmail.com>
2026-01-14 12:00:12 +0900

    fix : User Entity
    User Entity Column name 추가

--- src/main/java/dopamine/soundock/entity/User.java
+++ src/main/java/dopamine/soundock/entity/User.java
@@ -28,6 +28,7 @@ import java.time.LocalDateTime;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private int id;
 
     @NotBlank
