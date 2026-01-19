084878c093380eb3db90fe1049c0b2468f39a761
Younghun Park <yuki50397853@gmail.com>
2026-01-19 12:23:16 +0900

    fix : import 정리, loadUserByname 메서드 수정
    - 사용하지 않는 import 정리
    - "Role_"을 전부 대문자로 수정 ("ROLE_")

--- src/main/java/dopamine/soundock/global/JwtAuthenticationFilter.java
+++ src/main/java/dopamine/soundock/global/JwtAuthenticationFilter.java
@@ -1,12 +1,10 @@
 package dopamine.soundock.global;
 
-import dopamine.soundock.entity.AccessTokenBlacklist;
 import dopamine.soundock.repository.AccessTokenBlacklistRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,7 +15,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
