75cf98847f7c3c48d962d954d15fb68e0490d100
Younghun Park <yuki50397853@gmail.com>
2026-02-10 20:06:15 +0900

    refactor : 상태 변경 스케쥴러 수정
    - 상태 변경 스케쥴러에 승인 날짜 추가
    - 상태 변경 스케쥴러에 수혜자 관련 쿼리문 및 실행 추가

--- src/main/java/dopamine/soundock/scheduler/PopStatusScheduler.java
+++ src/main/java/dopamine/soundock/scheduler/PopStatusScheduler.java
@@ -23,14 +23,17 @@ public class PopStatusScheduler {
 
         try {
             // DONATION 처리 (현재 시간 - 3일 이전의 데이터), DB가 쿼리문이 실행되고 실제 변경된 데이터가 몇건인지 계산해서 반환해줌
-            int donationCount = popHistoryRepository.updateDonationStatus(now.minusDays(3));
+            int donationCount = popHistoryRepository.updateDonation(now, now.minusDays(3));
 
-            // FEATURED_BOARD 처리 (현재 시간 - 10분 이전의 데이터), DB가 쿼리문이 실행되고 실제 변경된 데이터가 몇건인지 계산해서 반환해줌
-            int boardCount = popHistoryRepository.updateBoardStatus(now.minusMinutes(10));
+            // RECEIVED 처리 (현재 시간 - 3일 이전의 데이터)
+            int receivedCount = popHistoryRepository.updateReceived(now.minusDays(3));
+
+            // FEATURED_BOARD 처리 (현재 시간 - 10분 이전의 데이터)
+            int boardCount = popHistoryRepository.updateBoard(now, now.minusMinutes(10));
 
             // 로그 대박 터지는거 방지하기 위해 실제로 처리가 됐을때만 로그 찍기
-            if (donationCount > 0 || boardCount > 0) {
-                log.info("pop 상태 처리 완료: DONATION: {}건, FEATURED_BOARD: {}건", donationCount, boardCount);
+            if (donationCount > 0 || receivedCount > 0 || boardCount > 0) {
+                log.info("pop 상태 처리 완료: DONATION: {}건, RECEIVED: {}건, FEATURED_BOARD: {}건", donationCount, receivedCount, boardCount);
             }
         } catch (Exception e) {
             log.error("PopStatus 정리 중 오류가 발생했습니다 : {}", e.getMessage(), e);
