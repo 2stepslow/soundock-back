50c0e38a7d4764fa632af481fe448df4f1920a4d
ning <xkfkr13@gmail.com>
2026-02-24 11:03:02 +0900

    security Config
    owasp zap 리포트 기반 Security config 정확한 도메인 경로만 허용으로 수정

--- src/main/java/dopamine/soundock/config/SecurityConfig.java
+++ src/main/java/dopamine/soundock/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList(
+        configuration.setAllowedOrigins(Arrays.asList(
                 frontendUrl,
                 albUrl
         ));
