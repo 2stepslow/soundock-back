f7d9a6893e3f50835191e861f90a2baebcb0e8cc
Younghun Park <yuki50397853@gmail.com>
2026-02-26 16:59:08 +0900

    refactor : 액세스토큰 로그 기록 코드 삭제
    - log로 액세스토큰을 찍는 코드 삭제 (로그 기록이 탈취당할 경우 위험할 수 있으므로)

--- src/main/java/dopamine/soundock/controller/AuthController.java
+++ src/main/java/dopamine/soundock/controller/AuthController.java
@@ -237,7 +237,6 @@ public class AuthController {
             @CookieValue(name = "refreshToken") String refreshToken // 쿠키에서 자동 추출
     ) {
         RefreshResponse response = authService.refresh(refreshToken);
-        log.info("{}", response.getAccessToken());
         return ResponseEntity.ok(RestResponse.success(response));
     }
 
